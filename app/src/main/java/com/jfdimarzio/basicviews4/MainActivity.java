8749a292c6887691af4425b62adc75b0d38277b4
Michael Trans <mich282q@edu.easj.dk>
2018-05-21 09:19:36 +0200

    with date

--- app/src/main/java/com/jfdimarzio/basicviews4/MainActivity.java
+++ app/src/main/java/com/jfdimarzio/basicviews4/MainActivity.java
@@ -1,14 +1,20 @@
 package com.jfdimarzio.basicviews4;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     TimePicker timePicker;
-
+    DatePicker datePicker;
+    int hour, minute;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,9 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
         timePicker = (TimePicker) findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);
-    }
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
 
-    public void onClick(View view) {
+    }
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener()
+
+            {
+ public void onTimeSet(
+         TimePicker view, int hourOfDay, int minuteOfHour)
+ {
+     hour = hourOfDay;
+     minute = minuteOfHour;
+     SimpleDateFormat timeFormat = new
+         SimpleDateFormat("hh:mm aa");
+
+     Date date = new Date();
+     String strDate = timeFormat.format(date);
+     Toast.makeText(getBaseContext(),
+          "You have selected " + strDate,
+          Toast.LENGTH_SHORT).show();
+      }
+ };
+
+                public void onClick(View view) {
         Toast.makeText(getBaseContext(),
                 "Time selected:" +
                         timePicker.getHour() +
